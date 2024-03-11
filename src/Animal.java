7f23d940d9457854d51b1ee2ecfc6a70dd00a151
herich <max.herich@gmail.com>
2024-03-11 10:19:19 +0100

    added setters and getters for class Animal

--- src/Animal.java
+++ src/Animal.java
@@ -5,6 +5,38 @@ public abstract class Animal {
     protected Size size;
     protected String specialCare;
 
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Size getSize() {
+        return size;
+    }
+
+    public void setSize(Size size) {
+        this.size = size;
+    }
+
+    public String getSpecialCare() {
+        return specialCare;
+    }
+
+    public void setSpecialCare(String specialCare) {
+        this.specialCare = specialCare;
+    }
+
     @Override
     public String toString() {
         return "Animal{" +
