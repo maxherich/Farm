e68f521f1bca7b6330ae789e9b41eeb3237ad383
herich <max.herich@gmail.com>
2024-03-11 10:48:41 +0100

    creating and adding counstructor for class Pig

---
+++ src/Pig.java
@@ -0,0 +1,8 @@
+public class Pig extends Animal{
+    public Pig(String type, String name, Size size, String specialCare) {
+        this.type = type;
+        this.name = name;
+        this.size = size;
+        this.specialCare = specialCare;
+    }
+}
