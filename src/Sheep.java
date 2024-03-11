2da183fafe24231b2c8ca5c78a1d29c3df6cc267
herich <max.herich@gmail.com>
2024-03-11 10:33:51 +0100

    added constructor for class Sheep

--- src/Sheep.java
+++ src/Sheep.java
@@ -1,2 +1,8 @@
-public class Sheep {
+public class Sheep extends Animal {
+    public Sheep(String type, String name, Size size, String specialCare) {
+        this.type = type;
+        this.name = name;
+        this.size = size;
+        this.specialCare = specialCare;
+    }
 }
