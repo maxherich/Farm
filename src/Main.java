8a54b273849ecf8679130c3d99eba2d1390fca52
herich <max.herich@gmail.com>
2024-03-11 10:57:11 +0100

    creating new Farm and adding animals and flowers

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
 
+        Farm farm = new Farm();
+        Sheep sheep = new Sheep("fourlegs", "Shawn", Size.SMALL, "care for wool");
+        Rose rose = new Rose("whiterose", 199.90, 5, 10);
+        Pig pig = new Pig("fourlegs", "Pep", Size.SMALL, "extra eat");
+        Sunflower sunflower = new Sunflower("yellowsunflower", 99.90, 10, 20);
+        farm.buy(sheep);
+        farm.plant(rose);
+        farm.buy(pig);
+        farm.plant(sunflower);
+        farm.toWater(0);
+        farm.caress(1);
+        System.out.println(farm);
     }
 }
