da0e5821984ce6885ba7c02709be6c0be230d20d
herich <max.herich@gmail.com>
2024-03-11 10:51:35 +0100

    creating and adding constructor for class Sunflower

---
+++ src/Sunflower.java
@@ -0,0 +1,8 @@
+public class Sunflower extends Flower{
+    public Sunflower(String name, double price, double neededArea, double chanceOfGrowth) {
+        this.name = name;
+        this.price = price;
+        this.neededArea = neededArea;
+        this.chanceOfGrowth = chanceOfGrowth;
+    }
+}
