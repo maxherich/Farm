0a0062c2714297b8e38b3aa8d806efd796e44a76
herich <max.herich@gmail.com>
2024-03-11 10:33:41 +0100

    added constructor for class Rose

--- src/Rose.java
+++ src/Rose.java
@@ -1,2 +1,8 @@
-public class Rose {
+public class Rose extends Flower {
+    public Rose(String name, double price, double neededArea, double chanceOfGrowth) {
+        this.name = name;
+        this.price = price;
+        this.neededArea = neededArea;
+        this.chanceOfGrowth = chanceOfGrowth;
+    }
 }
